27a2d294bef51d7dcb33f97b5044f9689d5a4682
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-11-03 11:36:06 -0800

    More minimal VecStorage

--- src/storage/vec.rs
+++ src/storage/vec.rs
@@ -3,11 +3,11 @@ use std::ptr;
 
 use super::Storage;
 
-pub struct VecStorage<T>(Vec<MaybeUninit<T>>);
+pub struct VecStorage<T>(Box<[MaybeUninit<T>]>);
 
 impl<T> Default for VecStorage<T> {
     fn default() -> Self {
-        Self(Vec::new())
+        Self(Vec::new().into())
     }
 }
 
@@ -17,7 +17,19 @@ impl<T: Send + 'static> Storage for VecStorage<T> {
     unsafe fn insert(&mut self, i: u32, x: Self::Component) {
         let i = i as usize;
         if i >= self.0.len() {
-            self.0.resize_with(i + 1, || MaybeUninit::uninit());
+            let new_len = if self.0.is_empty() {
+                128
+            } else {
+                self.0.len() * 2
+            };
+            let mut new = Vec::with_capacity(new_len);
+            new.resize_with(new_len, || MaybeUninit::uninit());
+            ptr::copy_nonoverlapping(
+                self.0.as_ptr() as *const T,
+                new.as_mut_ptr() as *mut T,
+                self.0.len(),
+            );
+            self.0 = new.into();
         }
         ptr::write(self.0[i].as_mut_ptr(), x);
     }
