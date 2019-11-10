9e1c442e70fafd8905953199bd23c9bf8089f75f
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-11-10 11:30:28 -0800

    Archetype rewrite

---
+++ src/archetype.rs
@@ -0,0 +1,167 @@
+use std::alloc::{alloc, Layout};
+use std::any::TypeId;
+use std::ptr::{self, NonNull};
+
+use fxhash::FxHashMap;
+
+use crate::{Component, ComponentSet};
+
+/// A block of dense components
+pub struct Archetype {
+    types: Vec<TypeInfo>,
+    offsets: FxHashMap<TypeId, usize>,
+    len: u32,
+    entities: Box<[u32]>,
+    data: Box<[u8]>,
+}
+
+impl Archetype {
+    pub fn new(types: Vec<TypeInfo>) -> Self {
+        Self {
+            types,
+            offsets: FxHashMap::default(),
+            entities: Box::new([]),
+            len: 0,
+            data: Box::new([]),
+        }
+    }
+
+    pub fn data<T: Component>(&self) -> Option<NonNull<T>> {
+        let offset = *self.offsets.get(&TypeId::of::<T>())?;
+        Some(unsafe {
+            NonNull::new_unchecked(self.data.as_ptr().add(offset).cast::<T>() as *mut T)
+        })
+    }
+    pub fn len(&self) -> usize {
+        self.len as usize
+    }
+
+    /// `index` must be in-bounds and live
+    pub unsafe fn get<T: Component>(&self, index: u32) -> &T {
+        debug_assert!(index < self.len);
+        &*self.data::<T>().unwrap().as_ptr().add(index as usize)
+    }
+
+    /// `index` must be in-bounds and live
+    pub unsafe fn get_mut<T: Component>(&mut self, index: u32) -> &mut T {
+        debug_assert!(index < self.len);
+        &mut *self.data::<T>().unwrap().as_ptr().add(index as usize)
+    }
+
+    /// Every type must be written immediately after this call
+    pub unsafe fn allocate(&mut self, id: u32) -> u32 {
+        if (self.len as usize) < self.entities.len() {
+            self.entities[self.len as usize] = id;
+            self.len += 1;
+            return self.len - 1;
+        }
+
+        // At this point we need to allocate more storage.
+        let count = if self.entities.len() == 0 {
+            64
+        } else {
+            self.entities.len() * 2
+        };
+        let mut new_entities = vec![!0; count].into_boxed_slice();
+        new_entities[0..self.entities.len()].copy_from_slice(&self.entities);
+        self.entities = new_entities;
+
+        let mut data_size = 0;
+        let mut offsets = FxHashMap::default();
+        for ty in &self.types {
+            data_size = align(data_size, ty.layout.align());
+            offsets.insert(ty.id, data_size);
+            data_size += ty.layout.size() * count;
+        }
+        let alloc = alloc(
+            Layout::from_size_align(
+                data_size,
+                self.types.first().map_or(1, |x| x.layout.align()),
+            )
+            .unwrap(),
+        );
+        let mut new_data = Box::from_raw(std::slice::from_raw_parts_mut(alloc, data_size));
+        if !self.data.is_empty() {
+            for ty in &self.types {
+                let old_off = *self.offsets.get(&ty.id).unwrap();
+                let new_off = *offsets.get(&ty.id).unwrap();
+                ptr::copy_nonoverlapping(
+                    self.data.as_ptr().add(old_off),
+                    new_data.as_mut_ptr().add(new_off),
+                    ty.layout.size() * self.entities.len(),
+                );
+            }
+        }
+
+        self.data = new_data;
+        self.offsets = offsets;
+        self.entities[self.len as usize] = id;
+        self.len += 1;
+        self.len - 1
+    }
+
+    pub unsafe fn remove(&mut self, index: u32) {
+        let last = self.len - 1;
+        for ty in &self.types {
+            let base = self
+                .data
+                .as_mut_ptr()
+                .add(*self.offsets.get(&ty.id).unwrap());
+            let removed = base.add(ty.layout.size() * index as usize);
+            (ty.drop)(removed);
+            if index != last {
+                ptr::copy_nonoverlapping(
+                    base.add(ty.layout.size() * last as usize),
+                    removed,
+                    ty.layout.size(),
+                );
+            }
+        }
+        if index != last {
+            self.entities[index as usize] = self.entities[last as usize];
+        }
+        self.len -= 1;
+    }
+
+    pub unsafe fn store(&mut self, set: impl ComponentSet) {
+        set.store(self.data.as_mut_ptr(), &self.offsets);
+    }
+}
+
+impl Drop for Archetype {
+    fn drop(&mut self) {
+        for i in 0..self.len {
+            if self.entities[i as usize] != !0 {
+                unsafe {
+                    self.remove(i);
+                }
+            }
+        }
+    }
+}
+
+fn align(x: usize, alignment: usize) -> usize {
+    assert!(alignment.is_power_of_two());
+    (x + alignment - 1) & (!alignment + 1)
+}
+
+#[derive(Debug)]
+pub struct TypeInfo {
+    id: TypeId,
+    layout: Layout,
+    drop: unsafe fn(*mut u8),
+}
+
+impl TypeInfo {
+    pub fn of<T: 'static>() -> Self {
+        unsafe fn drop_ptr<T>(x: *mut u8) {
+            x.cast::<T>().drop_in_place()
+        }
+
+        Self {
+            id: TypeId::of::<T>(),
+            layout: Layout::new::<T>(),
+            drop: drop_ptr::<T>,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,142 @@
-mod dynamic;
-mod storage;
+mod archetype;
 
-pub use dynamic::*;
-pub use storage::*;
+use std::any::TypeId;
 
-use hibitset::{BitSet, BitSetLike, BitSetNot};
+use downcast_rs::{impl_downcast, Downcast};
+use fxhash::FxHashMap;
+
+use archetype::{Archetype, TypeInfo};
+
+pub struct World {
+    entities: Vec<EntityMeta>,
+    free: Vec<u32>,
+    archetypes: Vec<Archetype>,
+    archetype_index: FxHashMap<Vec<TypeId>, usize>,
+}
+
+impl World {
+    pub fn new() -> Self {
+        Self {
+            entities: Vec::new(),
+            free: Vec::new(),
+            archetypes: Vec::new(),
+            archetype_index: FxHashMap::default(),
+        }
+    }
+
+    pub fn spawn(&mut self, components: impl ComponentSet) -> Entity {
+        use std::collections::hash_map::Entry;
+
+        let entity = match self.free.pop() {
+            Some(i) => Entity {
+                generation: self.entities[i as usize].generation,
+                id: i,
+            },
+            None => {
+                let i = self.entities.len() as u32;
+                self.entities.push(EntityMeta {
+                    generation: 0,
+                    archetype: 0,
+                    index: 0,
+                });
+                Entity {
+                    generation: 0,
+                    id: i,
+                }
+            }
+        };
+        let archetype = match self.archetype_index.entry(components.elements()) {
+            Entry::Occupied(x) => *x.get(),
+            Entry::Vacant(x) => {
+                self.archetypes.push(Archetype::new(components.info()));
+                let index = self.archetypes.len() - 1;
+                x.insert(index);
+                index
+            }
+        };
+        let archetype = &mut self.archetypes[archetype];
+        unsafe {
+            self.entities[entity.id as usize].index = archetype.allocate(entity.id);
+            archetype.store(components);
+        }
+        entity
+    }
+
+    pub fn despawn(&mut self, entity: Entity) -> bool {
+        let meta = &mut self.entities[entity.id as usize];
+        if meta.generation != entity.generation {
+            return false;
+        }
+        meta.generation += 1;
+        unsafe {
+            self.archetypes[meta.archetype as usize].remove(meta.index);
+        }
+
+        true
+    }
+
+    pub fn get<T: Component>(&self, entity: Entity) -> Option<&T> {
+        let meta = &self.entities[entity.id as usize];
+        if meta.generation != entity.generation {
+            return None;
+        }
+        unsafe { Some(self.archetypes[meta.archetype as usize].get(meta.index)) }
+    }
+
+    pub fn get_mut<T: Component>(&mut self, entity: Entity) -> Option<&mut T> {
+        let meta = &self.entities[entity.id as usize];
+        if meta.generation != entity.generation {
+            return None;
+        }
+        unsafe { Some(self.archetypes[meta.archetype as usize].get_mut(meta.index)) }
+    }
+}
+
+pub trait Component: Downcast + Send + Sync + 'static {}
+impl_downcast!(Component);
+impl<T: Send + Sync + 'static> Component for T {}
+
+struct EntityMeta {
+    generation: u32,
+    archetype: u32,
+    index: u32,
+}
 
 #[derive(Clone, Copy, Debug, Hash, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Entity {
     generation: u32,
-    index: u32,
+    id: u32,
 }
 
-#[derive(Default)]
-pub struct Entities {
-    mask: BitSet,
-    generations: Vec<u32>,
-}
-
-impl Entities {
-    fn spawn(&mut self) -> Entity {
-        let index = BitSetNot(&self.mask).iter().next().unwrap();
-        self.mask.add(index);
-        if index as usize >= self.generations.len() {
-            self.generations.resize(index as usize + 1, 0);
-        }
-        let generation = self.generations[index as usize];
-        Entity { generation, index }
-    }
-
-    /// Whether `entity` currently exists
-    fn contains(&self, entity: Entity) -> bool {
-        self.mask.contains(entity.index)
-            && self.generations[entity.index as usize] == entity.generation
-    }
-
-    /// Destroy an entity and all associated components
-    ///
-    /// Returns `false` iff the entity was previously destroyed
-    fn despawn(&mut self, entity: Entity) -> bool {
-        if !self.contains(entity) {
-            return false;
-        }
-        self.generations[entity.index as usize] =
-            self.generations[entity.index as usize].wrapping_add(1);
-        self.mask.remove(entity.index);
-        true
-    }
-}
-
-pub trait Fetch<T> {
-    type Ref;
-    fn fetch(self) -> Self::Ref;
+pub trait ComponentSet {
+    fn elements(&self) -> Vec<TypeId>;
+    fn info(&self) -> Vec<TypeInfo>;
+    unsafe fn store(self, base: *mut u8, offsets: &FxHashMap<TypeId, usize>);
 }
 
 macro_rules! tuple_impl {
-    ($($name:ident),*) => {
-        impl<'a, T, $($name),*> Fetch<($($name),*)> for &'a T
-            where $(Self: Fetch<$name>),*
-        {
-            type Ref = ($(<Self as Fetch<$name>>::Ref),*);
-            fn fetch(self) -> Self::Ref {
-                ($(<Self as Fetch<$name>>::fetch(self)),*)
+    ($($name: ident),*) => {
+        impl<$($name: Component),*> ComponentSet for ($($name,)*) {
+            fn elements(&self) -> Vec<TypeId> {
+                vec![$(TypeId::of::<$name>()),*]
+            }
+            fn info(&self) -> Vec<TypeInfo> {
+                vec![$(TypeInfo::of::<$name>()),*]
+            }
+            unsafe fn store(self, base: *mut u8, offsets: &FxHashMap<TypeId, usize>) {
+                #[allow(non_snake_case)]
+                let ($($name,)*) = self;
+                $(
+                    base.add(*offsets.get(&TypeId::of::<$name>()).unwrap())
+                        .cast::<$name>()
+                        .write($name);
+                )*
             }
         }
     }
 }
 
+tuple_impl!(A);
 tuple_impl!(A, B);
 tuple_impl!(A, B, C);
 tuple_impl!(A, B, C, D);
@@ -81,86 +151,24 @@ tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L);
 tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M);
 tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N);
 tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
-
-#[macro_export]
-macro_rules! world {
-    {
-        $(#[$meta:meta])*
-        $vis:vis struct $name:ident {
-            $($field:ident : $ty:ty),*$(,)?
-        }
-    } => {
-        #[derive(Default)]
-        struct Storages {
-            $($field : std::sync::Mutex<$crate::Masked<$ty>>,)*
-        }
-
-        $(#[$meta])*
-        $vis struct $name {
-            entities: Entities,
-            storages: Storages,
-        }
-
-        #[allow(dead_code)]
-        impl $name {
-            pub fn new() -> Self {
-                Self {
-                    entities: Entities::default(),
-                    storages: Storages::default()
-                }
-            }
-
-            pub fn entities(&self) -> &Entities {
-                &self.entities
-            }
-
-            /// Access one or more storages
-            pub fn get<'a, T>(&'a self) -> <&'a Self as Fetch<T>>::Ref
-            where
-                &'a Self: Fetch<T>,
-            {
-                self.fetch()
-            }
-
-            /// Create a new `Entity`
-            pub fn spawn(&mut self) -> Entity {
-                self.entities.spawn()
-            }
-
-            /// Whether `entity` currently exists
-            pub fn contains(&self, entity: Entity) -> bool {
-                self.entities.contains(entity)
-            }
-
-            /// Destroy an entity and all associated components
-            ///
-            /// Returns `false` iff the entity was previously destroyed
-            pub fn despawn(&mut self, entity: Entity) -> bool {
-                let was_live = self.entities.despawn(entity);
-                if was_live {
-                    $(
-                        let mut storage = self.storages.$field.try_lock().expect("storage already borrowed");
-                        storage.free(entity.index);
-                    )*
-                }
-                was_live
-            }
-        }
-
-        $(
-            impl<'a> Fetch<$ty> for &'a $name {
-                type Ref = std::sync::MutexGuard<'a, Masked<$ty>>;
-                fn fetch(self) -> std::sync::MutexGuard<'a, Masked<$ty>> {
-                    self
-                        .storages
-                        .$field
-                        .try_lock()
-                        .expect(concat!("storage ", stringify!($field), " already borrowed"))
-                }
-            }
-        )*
-    }
-}
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF);
+// tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG);
 
 #[cfg(test)]
 mod tests {
@@ -168,49 +176,9 @@ mod tests {
 
     #[test]
     fn smoke() {
-        let mut world = DynWorld::new();
-        world.register::<VecStorage<u32>>();
-        world.register::<VecStorage<u16>>();
-        let entity = world.spawn();
-        world.insert::<VecStorage<u32>>(entity, 32);
-        world.insert::<VecStorage<u16>>(entity, 16);
-
-        {
-            let s = world.get::<VecStorage<u32>>();
-            assert_eq!(s.iter().cloned().collect::<Vec<_>>(), [32]);
-
-            assert_eq!((&s, &s, &s).join().collect::<Vec<_>>(), [(&32, &32, &32)]);
-        }
-
-        {
-            let (s, mut t) = world.get::<(VecStorage<u32>, VecStorage<u16>)>();
-            assert_eq!((&s, &mut t).join().collect::<Vec<_>>(), [(&32, &mut 16)]);
-        }
-
-        assert!(world.contains(entity));
-        assert_eq!(world.remove::<VecStorage<u32>>(entity), Some(32));
-        assert_eq!(world.remove::<VecStorage<u32>>(entity), None);
-        assert!(world.despawn(entity));
-        assert!(!world.contains(entity));
-    }
-
-    #[test]
-    #[should_panic(expected = "already borrowed")]
-    fn double_borrow() {
-        let mut world = DynWorld::new();
-        world.register::<VecStorage<u32>>();
-        world.get::<(VecStorage<u32>, VecStorage<u32>)>();
-    }
-
-    #[test]
-    fn static_world() {
-        world! {
-            struct Static {
-                a: VecStorage<u32>,
-            }
-        }
-        let mut world = Static::new();
-        let e = world.spawn();
-        //world.get::<VecStorage<u32>>().insert(e, 32);
+        let mut world = World::new();
+        let e = world.spawn(("hi", 42));
+        assert_eq!(world.get::<&'static str>(e), Some(&"hi"));
+        assert_eq!(world.get::<i32>(e), Some(&42));
     }
 }
