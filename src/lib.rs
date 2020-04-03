552548986902995e87edbb108dc9aaa90dc85b65
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-04-03 11:52:01 -0700

    Reexport BatchedIter

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ pub use borrow::{EntityRef, Ref, RefMut};
 pub use bundle::{Bundle, DynamicBundle, MissingComponent};
 pub use entities::{Entity, NoSuchEntity};
 pub use entity_builder::{BuiltEntity, EntityBuilder};
-pub use query::{Access, Query, QueryBorrow, QueryIter, With, Without};
+pub use query::{Access, BatchedIter, Query, QueryBorrow, QueryIter, With, Without};
 pub use world::{ArchetypesGeneration, Component, ComponentError, Iter, SpawnBatchIter, World};
 
 // Unstable implementation details needed by the macros
