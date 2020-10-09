0e64189f1a54870610ba9f46ddb502f2d03fc82a
Sean Leffler <sean@errno.com>
2020-10-09 11:02:19 -0700

    Remove commented out code mess.

--- src/bundle.rs
+++ src/bundle.rs
@@ -25,9 +25,6 @@ pub trait DynamicBundle {
     /// Invoke a callback on the fields' type IDs, sorted by descending alignment then id
     #[doc(hidden)]
     fn with_ids<T>(&self, f: impl FnOnce(&[TypeId]) -> T) -> T;
-    // /// Invoke a callback on the fields' type IDs, sorted by descending alignment then id
-    // #[doc(hidden)]
-    // fn with_infos<T>(&self, f: impl FnOnce(&[TypeInfo]) -> T) -> T;
     /// Obtain the fields' TypeInfos, sorted by descending alignment then id
     #[doc(hidden)]
     fn type_info(&self) -> Vec<TypeInfo>;
@@ -43,9 +40,6 @@ pub trait DynamicBundle {
 pub trait Bundle: DynamicBundle {
     #[doc(hidden)]
     fn with_static_ids<T>(f: impl FnOnce(&[TypeId]) -> T) -> T;
-
-    // #[doc(hidden)]
-    // fn with_static_infos<T>(f: impl FnOnce(&[TypeInfo]) -> T) -> T;
 
     /// Obtain the fields' TypeInfos, sorted by descending alignment then id
     #[doc(hidden)]
@@ -86,10 +80,6 @@ impl std::error::Error for MissingComponent {}
 macro_rules! tuple_impl {
     ($($name: ident),*) => {
         impl<$($name: Component),*> DynamicBundle for ($($name,)*) {
-            // fn with_infos<T>(&self, f: impl FnOnce(&[TypeInfo]) -> T) -> T {
-            //     Self::with_static_infos(f)
-            // }
-
             fn with_ids<T>(&self, f: impl FnOnce(&[TypeId]) -> T) -> T {
                 Self::with_static_ids(f)
             }
@@ -129,13 +119,6 @@ macro_rules! tuple_impl {
                 xs.sort_unstable();
                 xs
             }
-
-            // fn with_static_infos<T>(f: impl FnOnce(&[TypeInfo]) -> T) -> T {
-            //     const N: usize = count!($($name),*);
-            //     let mut xs: [TypeInfo; N] = [$(TypeInfo::of::<$name>()),*];
-            //     xs.sort_unstable();
-            //     f(&xs)
-            // }
 
             #[allow(unused_variables, unused_mut)]
             unsafe fn get(mut f: impl FnMut(TypeInfo) -> Option<NonNull<u8>>) -> Result<Self, MissingComponent> {
