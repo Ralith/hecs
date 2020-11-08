9dde255ca41ccd92b54b2c23d73a1cd98b50b2cc
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-11-08 14:01:27 -0800

    Introduce convenience alias for query results

--- src/query_one.rs
+++ src/query_one.rs
@@ -1,7 +1,7 @@
 use core::marker::PhantomData;
 
 use crate::query::{Fetch, With, Without};
-use crate::{Archetype, Component, Query};
+use crate::{Archetype, Component, Query, QueryItem};
 
 /// A borrow of a `World` sufficient to execute the query `Q` on a single entity
 pub struct QueryOne<'a, Q: Query> {
@@ -32,7 +32,8 @@ impl<'a, Q: Query> QueryOne<'a, Q> {
     ///
     /// Panics if called more than once or if it would construct a borrow that clashes with another
     /// pre-existing borrow.
-    pub fn get(&mut self) -> Option<<Q::Fetch as Fetch<'_>>::Item> {
+    // Note that this uses self's lifetime, not 'a, for soundness.
+    pub fn get(&mut self) -> Option<QueryItem<'_, Q>> {
         if self.borrowed {
             panic!("called QueryOnce::get twice; construct a new query instead");
         }
