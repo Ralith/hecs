71f253372f15627ce581bf7fd8d4c8bf63c6c72e
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-12-04 20:53:08 -0800

    Make room for columnar serialization

---
+++ src/serialize/mod.rs
@@ -0,0 +1,11 @@
+//! Convenience tools for serializing [`World`](crate::World)s; requires the `serde` feature
+//!
+//! [`Component`](crate::Component)s are not necessarily serializable, so we cannot directly
+//! implement [`serde::Serialize`] for [`World`](crate::World). The helpers defined in this module
+//! allow serialization and deserialization based on purpose-defined traits to control the
+//! procedures explicitly.
+//!
+//! Backwards-incompatible changes to the serde data models herein are subject to the same semantic
+//! versioning stability guarantees as the hecs API.
+
+pub mod row;
