13550173491b4ccd62fabb08fe2d86221775e9f9
KBrock <All8Up@gmail.com>
2022-03-15 22:18:58 -0400

    Prefered solution.

--- src/parallel.rs
+++ src/parallel.rs
@@ -19,7 +19,7 @@
 // replicates the borrow/lifetime rules but rather than the language
 // rules done on control flow, they will be replicating them in the
 // time domain of the threading system and job lifetimes.
-// 
+//
 use {
     super::{entities::EntityMeta, Archetype, Entity, Fetch, Query, QueryItem},
     std::sync::{
@@ -324,5 +324,158 @@ impl<'a, Q: Query> Iterator for ParallelIter<'a, Q> {
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         self.next_in_range()
+    }
+}
+
+/*
+This is an alternative parallel iteration approach.  Personally even though it does not match
+the style of the iterator based solutions, it is hugely better for many reasons.  Primary among
+the benefits is far less unsafe behavior, it is tied to the world dynamically meaning that it
+does not have to carry references into the parallel execution graphs and it's a ton less code.
+ */
+
+//
+pub(crate) struct ParIterParts {
+    archetype: u32,
+    index: u32,
+}
+
+impl ParIterParts {
+    /// Create a shared parts structure.
+    /// `archetype` is the index of the archetype in `self.archetypes` which is being iterated.
+    /// `index` is the entry index into the current archetype.
+    pub fn with(archetype: usize, index: usize) -> Self {
+        Self {
+            archetype: archetype as u32,
+            index: index as u32,
+        }
+    }
+
+    /// Extract the archetype index from the parts.
+    #[inline]
+    pub fn archetype(&self) -> usize {
+        self.archetype as usize
+    }
+
+    /// Extract the index from the parts.
+    #[inline]
+    pub fn index(&self) -> usize {
+        self.index as usize
+    }
+}
+
+impl From<u64> for ParIterParts {
+    fn from(value: u64) -> Self {
+        Self {
+            archetype: (value >> 32) as u32,
+            index: value as u32,
+        }
+    }
+}
+
+impl From<ParIterParts> for u64 {
+    fn from(parts: ParIterParts) -> Self {
+        (parts.archetype as u64) << 32 | parts.index as u64
+    }
+}
+
+/// An opaque parallel iteration handle.
+#[derive(Debug, Clone)]
+pub struct ParIter(Arc<AtomicU64>);
+
+impl ParIter {
+    /// Create a parallel iterator.
+    /// This is an opaque tracking type which does not maintain any borrows,
+    /// does not contain the query type or anything else.  Versus the iterator
+    /// solution this makes it extremely 'safe'.
+    pub fn new() -> Self {
+        Self(Arc::new(AtomicU64::new(0)))
+    }
+
+    /// Perform an atomic load and return a shared parts structure.
+    #[inline]
+    pub(crate) fn load(&self) -> ParIterParts {
+        let value = self.0.load(Ordering::Acquire);
+        value.into()
+    }
+
+    /// Store a new value into the atomic.
+    #[inline]
+    pub fn store(&self, old: u64, new: u64) {
+        let _ = self
+            .0
+            .compare_exchange(old, new, Ordering::Acquire, Ordering::Relaxed);
+    }
+
+    /// Execute the query.
+    pub(crate) fn execute<'a, Q: Query>(
+        &self,
+        meta: &[EntityMeta],
+        archetypes: &[Archetype],
+        partition_size: usize,
+        func: &dyn Fn(Entity, QueryItem<'a, Q>),
+    ) {
+        // Loop until we run out of archetypes to process.
+        loop {
+            let shared = self.load();
+            let mut shared_archetype = shared.archetype();
+
+            if shared_archetype < archetypes.len() {
+                if let Some(archetype_state) = Q::Fetch::prepare(&archetypes[shared_archetype]) {
+                    // This is a valid archetype to iterate, attempt to claim a range of entries to process.
+                    let archetype = &archetypes[shared_archetype];
+                    let start = shared.index();
+
+                    if start < archetype.len() as usize {
+                        // Compute the end, clamping it to the end of the archetype.
+                        let end = (start + partition_size).min(archetype.len() as usize);
+
+                        // Attempt to take the range.
+                        match self.0.compare_exchange(
+                            shared.into(),
+                            ParIterParts::with(shared_archetype, end).into(),
+                            Ordering::Acquire,
+                            Ordering::Relaxed,
+                        ) {
+                            Ok(_) => {
+                                // All good.
+                                // Iterate the entities+components.
+                                let fetch = Q::Fetch::execute(archetype, archetype_state);
+                                let entities = archetype.entities().as_ptr();
+
+                                for index in start..end {
+                                    unsafe {
+                                        let entity = *entities.add(index);
+                                        let entity = Entity {
+                                            id: entity,
+                                            generation: meta
+                                                .get_unchecked(entity as usize)
+                                                .generation,
+                                        };
+                                        (func)(entity, fetch.get(index));
+                                    }
+                                }
+                                continue;
+                            }
+                            Err(_) => {
+                                // Try again from the top.
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                // Invalid archetype.  Try the next one.
+                shared_archetype += 1;
+                // Success or fail, restart the loop.
+                self.store(
+                    shared.into(),
+                    ParIterParts::with(shared_archetype, 0).into(),
+                );
+            } else {
+                // Done with the iteration.
+                break;
+            }
+        }
     }
 }
