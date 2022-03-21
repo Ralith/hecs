f5cdf626626f309f59e5e612dcf9ff82d7a196f0
KBrock <All8Up@gmail.com>
2022-03-20 22:25:23 -0400

    The mother of all examples...

--- examples/ffa_parallel/ffa.rs
+++ examples/ffa_parallel/ffa.rs
@@ -1,6 +1,11 @@
 use hecs::*;
 use rand::{thread_rng, Rng};
-use std::io;
+use std::{
+    any::TypeId,
+    collections::{BTreeMap, BTreeSet},
+    io,
+    sync::Mutex,
+};
 
 /*
  Modified example of the ffa_simulation.rs example for parallel execution.
@@ -297,26 +302,91 @@ pub fn main() {
 
 /*
 In order to run an ECS in a graph executor, a topological sort is executed on
-the systems based on the data which they access and if that access is mutable
-or immutable.  The goal is to end up with a generic list of execution groups
-which can be instanced once a frame.
+the systems.  The sort is based on normal Rust borrow rules such that at any
+time during execution there is never a mutable reference to data at the same
+time as any immutable reference.  The topological sort generates a DAG which
+is used to determine execution order which maintains the required constraints.
+For the purposes here, the sorting is performed at time of system insertion
+and rather than an actual graph, systems are grouped in depth ordered vectors.
+
+Since hecs does not have a concept of shared components, a few fake components
+are used to represent external to world data flows.  For instance, the damage
+queue which is written to within the nearest search and read from within the
+apply damage systems.
+
+Provided here is the high level concept of a full threaded scheduler.  While
+not complete or using a real graph executor, the concepts are all in place.
+Using this as the starting point for a full blown threaded execution of the
+ECS is viable, though to make it ergonomic and safer in practice requires a
+fair amount of additional abstraction.
+
+ECS execution within a work stealing job system is not a great solution to
+the backend threading.  A work stealing job system is great for a large number
+of types of work but the overhead for job management increases greatly
+for the patterns used in an ECS.  Using a proper graph executor rather than
+the current example based on Rayon can provide considerably higher performance
+and scalability, unfortunately this is already much larger as an example than
+originally intended.
  */
 
-// A trait representing a system which can be executed on the an immutable
-// world.
-trait SystemCall {
+// A system which executes on a single thread.
+trait SerialCall: Sync {
     fn call(&self, world: &World);
 }
 
+// A system which is executed in parallel on several threads.
+trait ParallelCall: Sync {
+    fn call(&self, world: &World, iter: ParallelIter);
+}
+
+// Struct for storing serial systems.
+struct Serial<'a, Q: Query + 'static> {
+    pub f: &'a fn(&World, Entity, QueryItem<Q>),
+}
+
+impl<'a, Q: Query + 'static> SerialCall for Serial<'a, Q> {
+    fn call(&self, world: &World) {
+        let mut borrow = world.query::<Q>();
+        for item in borrow.into_iter() {
+            (*self.f)(world, item.0, item.1);
+        }
+    }
+}
+
+unsafe impl<'a, Q: Query + 'static> Sync for Serial<'a, Q> {}
+
+// Struct for storing parallel systems.
+struct Parallel<'a, Q: Query + 'static> {
+    f: &'a fn(&World, Entity, QueryItem<Q>),
+}
+
+impl<'a, Q: Query + 'static> ParallelCall for Parallel<'a, Q> {
+    fn call(&self, world: &World, iter: ParallelIter) {
+        unsafe {
+            world.parallel_query::<Q>(iter, 100, &|entity, item| (*self.f)(world, entity, item))
+        };
+    }
+}
+
+unsafe impl<'a, Q: Query + 'static> Sync for Parallel<'a, Q> {}
+
+// Execution style enum.  Systems can be executed serial or parallel depending
+// on requirements.  Not all systems can be made fully parallel, but they can
+// usually still run in parallel with other systems depending on data access.
+enum Execution {
+    Serial(Box<dyn SerialCall>),
+    Parallel(Box<dyn ParallelCall>),
+}
+
 // An entry in the schedule representing either an iteration or a command buffer
-// flush.  Individually these are synchronized so there is no overlapping execution.
+// flush.
 enum Entry {
-    Immutable(Vec<Box<dyn SystemCall>>),
+    Systems(Vec<Execution>),
     Flush,
 }
 
 // A compiled schedule.
-struct Schedule(Vec<Entry>);
+struct Schedule(Vec<Entry>, Mutex<CommandBuffer>);
 
 impl Schedule {
     fn create() -> ScheduleBuilder {
@@ -324,27 +394,77 @@ impl Schedule {
     }
 
     fn new() -> Self {
-        Self(Vec::new())
+        Self(Vec::new(), Mutex::new(CommandBuffer::new()))
     }
 
     pub fn execute(&self, world: &mut World) {
-        // Using Rayon is not exactly the best example but I was unable to find
-        // a threaded graph executor to use for demo purposes.  The issue with
-        // this implementation is that it will end up dynamically building jobs
-        // rather than issuing the entire graph at one time.  I'll fake it up
-        // but there are much better solutions for games.
-        for entry in &self.0 {
-            // Each entry represents a layer in the DAG produced by topological
-            // sorting of the system data access.  The layers can not overlap
-            // but everything within the layer can execute simultaneously.
-        }
+        // Using Rayon is not the best solution for graph execution but there
+        // was no existing crate with a graph executor which was available.
+        rayon::scope(|_| {
+            // This scope represents the overall execution of the entire schedule.
+            // In rayon, this is an issued job to the backend which will block the
+            // calling thread until the entire schedule completes.  AKA: fork/join
+            // in order to maintain Rust "scoping" on the main thread.
+
+            // During schedule execution, this scope owns the mutable world reference.
+            let world = world;
+
+            // Each loop issues a new scope.  This is equivalent to what a graph
+            // executor would do in terms that it issues the system calls then a
+            // fence/barrier or other synchronization point.
+            for entry in &self.0 {
+                match entry {
+                    Entry::Systems(systems) => {
+                        // The list of systems which can run in parallel at this level
+                        // of traversing the dag.
+                        rayon::scope(|scope| {
+                            // Use the immutable world within this scope.
+                            let world: &World = world;
+                            for system in systems {
+                                match system {
+                                    Execution::Serial(serial) => {
+                                        // Issue a single job for this.
+                                        scope.spawn({
+                                            let world: &World = world;
+                                            move |_| {
+                                                serial.call(world);
+                                            }
+                                        });
+                                    }
+                                    Execution::Parallel(parallel) => {
+                                        // Execute x jobs.
+                                        let iter = ParallelIter::new();
+                                        for _ in 0..rayon::current_num_threads() {
+                                            scope.spawn({
+                                                let world: &World = world;
+                                                let iter = iter.clone();
+                                                move |_| {
+                                                    parallel.call(world, iter);
+                                                }
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    Entry::Flush => {
+                        // Flush the command buffer.
+                        rayon::scope({
+                            let world: &mut World = world;
+                            move |_| {
+                                let mut commands = self.1.lock().unwrap();
+                                commands.run_on(world);
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 }
 
-use std::{
-    any::TypeId,
-    collections::{BTreeMap, BTreeSet},
-};
+/// A component set which is used during topological sorting.
 struct ComponentSet(BTreeMap<TypeId, bool>);
 
 impl ComponentSet {
@@ -379,13 +499,13 @@ impl ComponentSet {
         for entry in &self.0 {
             if *entry.1 {
                 // If the lhs type is mutable, the rhs is incompatible if it contains
-                // the same type of any mutability it is incompatible.
+                // the same typeid no matter what it's mutability.
                 if rhs.0.contains_key(entry.0) {
                     return false;
                 }
             } else {
                 // The lhs is immutable, the rhs is only incompatible if it contains
-                // a mutable of the same type.
+                // a mutable of the same typeid.
                 if let Some(rhs) = rhs.0.get_key_value(entry.0) {
                     if *rhs.1 {
                         return false;
@@ -396,18 +516,15 @@ impl ComponentSet {
         false
     }
 
-    // A union of the two sets of keys with any combination of the mutability
-    // being true resulting in a true result.
+    // A union of the two sets of keys where the mutability is the logical
+    // `or` of the two sides.
     pub fn merge(&self, rhs: &ComponentSet) -> Self {
         // Resulting map with mutability raised to highest value.
         let mut result = self.0.clone();
 
         for (k, v) in &rhs.0 {
             if let Some(e) = result.get_mut(k) {
-                // If either is true, result is true.
-                if !v && *e {
-                    *e = true;
-                }
+                *e = *v || *e;
             } else {
                 result.insert(*k, *v);
             }
@@ -426,14 +543,18 @@ impl ScheduleBuilder {
         Self { dag: Vec::new() }
     }
 
-    // This reuses the hecs query type rather than introducing a bunch of
-    // additional wrapper code to make it all pretty and de-duplicated.
-    // In the query, list all components (and fake ones) which the system
-    // can access.  This list will be used for the topological insert sort.
-    // A full solution would prevent direct access to the world and would
-    // check any internal queries were valid against this list.
-    pub fn immutable<Q: Query>(mut self, call: &dyn SystemCall) -> Self {
+    // Two queries here.  The `Q` query type represents the set of component arguments
+    // to the system function.  The 'D' query is used to represent any other components
+    // which the execution of the system might end up touching.  There are no protections
+    // against incorrect lists of components here, a full solution would check for
+    // validity.
+    pub fn serial<Q: Query + 'static, D: Query>(
+        mut self,
+        f: &'static fn(&World, Entity, QueryItem<Q>),
+    ) -> Self {
+        // Get a set of all components in use.
         let components = ComponentSet::from_query::<Q>();
+        let components = components.merge(&ComponentSet::from_query::<D>());
 
         // Iterate from the end checking if this new system is compatible.
         let mut compatible: Option<usize> = None;
@@ -442,18 +563,31 @@ impl ScheduleBuilder {
             if target.0.is_empty() || !components.is_compatible(&target.0) {
                 break;
             }
-            compatible = Some(len - index);
+            compatible = Some(len - index - 1);
         }
 
-        if let Some(index) = compatible {
+        // Insert it.
+        self.insert(
+            compatible,
+            Execution::Serial(Box::new(Serial::<'_, Q> { f })),
+        );
+        self
+    }
+
+    fn insert(&mut self, index: Option<usize>, execution: Execution) {
+        if let Some(index) = index {
             // Insert into the last compatible group we found.
-            //self.dag[index].1.push(Entry::Immutable(vec![Box::new(call)]));
+            match &mut self.dag[index].1 {
+                Entry::Systems(systems) => {
+                    systems.push(execution);
+                }
+                _ => panic!("Internal error."),
+            }
         } else {
             // Just push to the end.
-            //self.dag.push((ComponentSet::new(), Entry::Immutable(vec![Box::new(call)])));
+            self.dag
+                .push((ComponentSet::new(), Entry::Systems(vec![execution])));
         }
-
-        self
     }
 
     pub fn flush(mut self) -> Self {
