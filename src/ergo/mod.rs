0a808fac1a59655aff2e6855d7f3c7188031542c
Karl Bergström <karl.anton.bergstrom@gmail.com>
2022-08-14 20:07:33 +0200

    Export Entity in hecs::ergo

--- src/ergo/mod.rs
+++ src/ergo/mod.rs
@@ -2,7 +2,7 @@ mod access;
 mod query;
 mod scope;
 
-pub use crate::World;
+pub use crate::{Entity, World};
 pub use access::ComponentRef;
 pub use query::{Or, Query, QueryBorrow, QueryItem, QueryIter, Satisfies, With, Without};
 pub use scope::ErgoScope;
