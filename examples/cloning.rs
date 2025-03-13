f0fbf907480898ed25109d6f55f52221c14f9427
Caspar Krieger <git@asparck.com>
2025-03-13 19:57:55 +0800

    Clarify/caveat iteration order of cloned world
    And add a couple more asserts to demonstrate expected behavior

--- examples/cloning.rs
+++ examples/cloning.rs
@@ -3,7 +3,11 @@
 //!
 //! By using [`World::set_freelist()`], we ensure that the cloned world will allocate new entities
 //! in sync with the original world: newly allocated entities will have the same entity ids as the
-//! original world, and the iteration order will be the same.
+//! original world.
+//!
+//! Iteration order of entities in the cloned world will be the same as the original world, but this
+//! is only incidentally the case and not necessarily guaranteed by the API in use here; see
+//! comments in the implementation of [`WorldCloner::clone_world()`] for more details.
 
 use hecs::{Archetype, ColumnBatchBuilder, ColumnBatchType, Component, TypeIdMap, TypeInfo, World};
 use std::any::TypeId;
@@ -15,12 +19,8 @@ struct ComponentCloneMetadata {
 
 /// Clones world entities along with registered components when [Self::clone_world()] is called.
 ///
-/// Unregistered components are omitted from the cloned world. Entities containing unregistered
-/// components will still be cloned.
-///
-/// Note that entity allocator state may differ in the cloned world - so for example a new entity
-/// spawned in each world may end up with different entity ids, entity iteration order may be
-/// different, etc.
+/// Unregistered components are omitted from the cloned world, but entities containing unregistered
+/// components will still be cloned (even if they result in "empty" entities).
 #[derive(Default)]
 struct WorldCloner {
     registry: TypeIdMap<ComponentCloneMetadata>,
@@ -47,6 +47,14 @@ impl WorldCloner {
 
         let mut cloned = World::new();
 
+        // Copy over archetypes by invoking the stored cloning function for each component type.
+        //
+        // Iteration order of the cloned world is (incidentally) preserved here because iterating
+        // through the world (or iterating through the results of a query on the world) iterates
+        // through all archetypes in the order in which they were added to the world - so to keep
+        // iteration order the same, we rely on `archetypes()` returning archetypes in the order
+        // they were added to the world, and we make sure to add those to the cloned world in that
+        // same order again.
         for archetype in world.archetypes() {
             let mut batch_type = ColumnBatchType::new();
             for (&type_id, clone_metadata) in self.registry.iter() {
@@ -109,6 +117,7 @@ pub fn main() {
         "cloned world should have same entity count as original world"
     );
 
+    // check iteration order is the same via iterating over all entities
     for (original_entity, cloned_entity) in world0.iter().zip(world1.iter()) {
         assert_eq!(
             original_entity.entity(),
@@ -119,6 +128,24 @@ pub fn main() {
         let cloned_i32 = cloned_entity.get::<&i32>().unwrap();
         assert_eq!(*original_i32, *cloned_i32, "i32 component should be cloned");
     }
+
+    // check iteration order is the same via a query
+    let mut query = world0.query::<&i32>();
+    let mut cloned_query = world1.query::<&i32>();
+    for ((original_entity, original_i32), (cloned_entity, cloned_i32)) in
+        query.iter().zip(cloned_query.iter())
+    {
+        assert_eq!(
+            original_entity, cloned_entity,
+            "query entity iteration order should be the same"
+        );
+        assert_eq!(
+            *original_i32, *cloned_i32,
+            "query i32 components should be the same"
+        );
+    }
+    drop(query);
+    drop(cloned_query);
 
     // spawn something to make sure that entity ids allocated are the same
     let entity1 = world0.spawn(entity1_data.clone());
@@ -148,7 +175,11 @@ pub fn main() {
 
     // but the u8 component shouldn't have been cloned
     assert!(
+        world1.contains(entity4),
+        "entity 4 should be in the cloned world despite containing only unregistered components"
+    );
+    assert!(
         world1.get::<&u8>(entity4).is_err(),
-        "u8 component should not be cloned because it was not registered"
+        "entity 4's u8 component should not be cloned because it was not registered"
     );
 }
