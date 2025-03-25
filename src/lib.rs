659431e30270e8b1becb35b7ee44d7183b69e200
Benjamin Saunders <ben.e.saunders@gmail.com>
2025-03-24 18:40:26 -0700

    Remove Google boilerplate
    Google has disclaimed ownership of hecs copyright, so we can tidy up.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 //! A handy ECS
 //!
 //! hecs provides a high-performance, minimalist entity-component-system (ECS) world. It is a
